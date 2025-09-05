0d8e6a4cfac263e992b40c54f892e8fb32d0d716
Raphael Lima <raphaeldesousalm@gmail.com>
2025-09-05 09:44:53 -0300

    feat: add button confirm password

--- src/pages/singupPage.jsx
+++ src/pages/singupPage.jsx
@@ -1,7 +1,6 @@
-import { EyeIcon, EyeOffIcon } from 'lucide-react'
-import { useState } from 'react'
 import { Link } from 'react-router'
 
+import PasswordInput from '@/components/password-input'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -14,8 +13,6 @@ import {
 import { Input } from '@/components/ui/input'
 
 const SingupPage = () => {
-  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
-
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-3">
       <Card className="w-[500px]">
@@ -27,19 +24,8 @@ const SingupPage = () => {
           <Input placeholder="Digite seu nome" />
           <Input placeholder="Digite seu sobrenome" />
           <Input placeholder="Digite seu email" />
-          <div className="relative">
-            <Input
-              type={passwordIsVisible ? 'text' : 'password'}
-              placeholder="Digite sua senha"
-            />
-            <Button
-              variant="ghost"
-              className="absolute bottom-0 right-0 top-0 my-auto mr-1 h-8 w-8 text-muted-foreground"
-              onClick={() => setPasswordIsVisible((prev) => !prev)}
-            >
-              {passwordIsVisible ? <EyeOffIcon /> : <EyeIcon />}
-            </Button>
-          </div>
+          <PasswordInput />
+          <PasswordInput placeholder="Digite sua senha novamente" />
         </CardContent>
         <CardFooter>
           <Button className="w-full">Criar conta</Button>
