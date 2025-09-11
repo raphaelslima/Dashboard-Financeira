0ec25b8d49190ce90b115018f2edd7377419fa6c
Raphael Lima <raphaeldesousalm@gmail.com>
2025-09-11 08:46:11 -0300

    chore: add react-query setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import './index.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
@@ -9,15 +10,19 @@ import LoginPage from './pages/loginPage'
 import NotFoundPage from './pages/notFoundPage'
 import SingupPage from './pages/singupPage'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<HomePage />} path="/" />
-        <Route element={<LoginPage />} path="/login" />
-        <Route element={<SingupPage />} path="/singup" />
-        <Route element={<NotFoundPage />} path="*" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<HomePage />} path="/" />
+          <Route element={<LoginPage />} path="/login" />
+          <Route element={<SingupPage />} path="/singup" />
+          <Route element={<NotFoundPage />} path="*" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
