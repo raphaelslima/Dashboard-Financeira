bc16ae98847fc334709d3aead821036022979ba4
Raphael Lima <raphaeldesousalm@gmail.com>
2025-09-11 09:50:45 -0300

    feat: validade confirm password

--- src/pages/singupPage.jsx
+++ src/pages/singupPage.jsx
@@ -28,32 +28,42 @@ import {
 import { Input } from '@/components/ui/input'
 import { api } from '@/lib/axios'
 
-const singupSchema = z.object({
-  first_name: z.string().trim().min(1, {
-    message: 'O nome é obrigatório.',
-  }),
-  last_name: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório.',
-  }),
-  email: z
-    .string()
-    .email({
-      message: 'O email é inválido',
-    })
-    .trim()
-    .min(1, {
+const singupSchema = z
+  .object({
+    first_name: z.string().trim().min(1, {
       message: 'O nome é obrigatório.',
     }),
-  password: z.string().trim().min(6, {
-    message: 'A senha precisa ter no mínimo 6 caracteres.',
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'Confirmação de senha é obrigatória.',
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Aceite os termos para efetuar o cadastro.',
-  }),
-})
+    last_name: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório.',
+    }),
+    email: z
+      .string()
+      .email({
+        message: 'O email é inválido',
+      })
+      .trim()
+      .min(1, {
+        message: 'O nome é obrigatório.',
+      }),
+    password: z.string().trim().min(6, {
+      message: 'A senha precisa ter no mínimo 6 caracteres.',
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'Confirmação de senha é obrigatória.',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Aceite os termos para efetuar o cadastro.',
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirmation
+    },
+    {
+      message: 'As senhas não coincidem',
+      path: ['passwordConfirmation'],
+    }
+  )
 
 const SingupPage = () => {
   const [user, setUser] = useState(null)
