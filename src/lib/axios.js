54a888b575a272c654edd4cb6ebf1face2271dd7
Raphael Lima <raphaeldesousalm@gmail.com>
2025-09-18 19:22:34 -0300

    feat: ensure acess token is refresh using refresh token on 401 error

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-import { LOCAL_STORAGE_ACCESS_TOKEN } from '@/constants/localStorage'
+import {
+  LOCAL_STORAGE_ACCESS_TOKEN,
+  LOCAL_STORAGE_REFRESH_TOKEN,
+} from '@/constants/localStorage'
 
 export const publicApi = axios.create({
   baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api',
@@ -19,3 +22,33 @@ protectApi.interceptors.request.use((request) => {
   request.headers.Authorization = `Bearer ${acessToken}`
   return request
 })
+
+protectApi.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const request = error.config.request
+    const refreshToken = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN)
+    if (!refreshToken) return Promise.reject(error)
+
+    if (
+      error.response.status === 401 &&
+      !request._retry &&
+      !request.url.includes('/auth/refresh')
+    ) {
+      request._retry = true
+      const response = await protectApi.post('/auth/refresh', {
+        refreshToken,
+      })
+      try {
+        const newAcessToken = response.data.acessToken
+        const newRefreshToken = response.data.refreshToken
+        localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN, newAcessToken)
+        localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN, newRefreshToken)
+        request.headers.Authorization = `Bearer ${newAcessToken}`
+        return protectApi(request)
+      } catch (refreshError) {
+        console.error(refreshError)
+      }
+    }
+  }
+)
