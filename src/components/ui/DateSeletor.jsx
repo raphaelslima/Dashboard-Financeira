e15c391e28b149bc24043d8ccfdf97c3e99ffa7a
Raphael Lima <raphaeldesousalm@gmail.com>
2025-09-22 17:39:55 -0300

    feat: add set date on URL

--- src/components/ui/DateSeletor.jsx
+++ src/components/ui/DateSeletor.jsx
@@ -1,13 +1,30 @@
-import { addMonths } from 'date-fns'
-import { useState } from 'react'
+import { addMonths, format } from 'date-fns'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
 
 import DatePickerWithRanger from './DatePickerWithRanger'
 
+const formatDate = (date) => {
+  return format(date, 'yyyy-MM-dd')
+}
+
 const DateSeletor = () => {
+  const navigate = useNavigate()
   const [date, setDate] = useState({
     from: new Date(),
     to: addMonths(new Date(), 1),
   })
+
+  useEffect(() => {
+    if (!date.from && !date.to) return
+
+    const queryParams = new URLSearchParams()
+    queryParams.set('from', formatDate(date.from))
+    queryParams.set('to', formatDate(date.to))
+
+    navigate(`/?${queryParams.toString()}`)
+  }, [navigate, date])
+
   return <DatePickerWithRanger value={date} onChange={setDate} />
 }
 
