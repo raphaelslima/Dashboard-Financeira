5feeaa441ffdddb0889686d4d0b654477fdc0f43
Raphael Lima <raphaeldesousalm@gmail.com>
2025-09-26 15:58:50 -0300

    fix: ref dataPicker

--- src/components/ui/DatePicker.jsx
+++ src/components/ui/DatePicker.jsx
@@ -1,6 +1,7 @@
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale/pt-BR'
 import { Calendar as CalendarIcon } from 'lucide-react'
+import { forwardRef } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
@@ -10,37 +11,38 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover'
 
-const DatePicker = ({
-  value,
-  onChange,
-  placeholder = 'Selecione uma data',
-}) => {
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          data-empty={!value}
-          className="w-full justify-start text-left font-normal data-[empty=true]:text-muted-foreground"
-        >
-          <CalendarIcon />
-          {value ? (
-            format(value, 'PPP', { locale: ptBR })
-          ) : (
-            <span>{placeholder}</span>
-          )}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
-          mode="single"
-          selected={value}
-          onSelect={onChange}
-          locale={ptBR}
-        />
-      </PopoverContent>
-    </Popover>
-  )
-}
+const DatePicker = forwardRef(
+  ({ value, onChange, placeholder = 'Selecione uma data' }, ref) => {
+    return (
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            ref={ref}
+            variant="outline"
+            data-empty={!value}
+            className="w-full justify-start text-left font-normal data-[empty=true]:text-muted-foreground"
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {value ? (
+              format(value, 'PPP', { locale: ptBR })
+            ) : (
+              <span>{placeholder}</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={value}
+            onSelect={onChange}
+            locale={ptBR}
+          />
+        </PopoverContent>
+      </Popover>
+    )
+  }
+)
+
+DatePicker.displayName = 'DatePicker'
 
 export default DatePicker
