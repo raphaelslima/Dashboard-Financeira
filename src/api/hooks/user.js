ae4205a63e072fc2a1764553acf305b50e2b59e5
Raphael Lima <raphaeldesousalm@gmail.com>
2025-09-29 18:09:16 -0300

    fix: query invalid

--- src/api/hooks/user.js
+++ src/api/hooks/user.js
@@ -3,12 +3,10 @@ import { useQuery } from '@tanstack/react-query'
 import { userServices } from '@/api/services/user'
 import { useAuthContext } from '@/context/auth'
 
-export const getUserBalanceQueryKey = (userId, from, to) => [
-  'balance',
-  userId,
-  from,
-  to,
-]
+export const getUserBalanceQueryKey = (userId, from, to) => {
+  if (!from || !to) return ['balance', userId]
+  return ['balance', userId, from, to]
+}
 
 export const useGetUserBalance = ({ from, to }) => {
   const { user } = useAuthContext()
